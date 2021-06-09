c5e1aa10d37900a6172ae34354ad62048c0b08f0
Innokentii <bopuclav42@gmail.com>
2021-06-09 18:45:16 +0300

    Single amd multi line text

--- watermark.py
+++ watermark.py
@@ -3,15 +3,22 @@ from PIL import ImageDraw, Image, ImageFont
 
 # на один символ по горизонтали 0.0165
 
-def craete_watermark(font=None,text_size=1,lines_number=1,rotation=None,direction='ltr',opacity=128,backgrount_as_image=False):
+
+def craete_watermark(font=None,text_size_cust=1,lines_number=1,rotation=None,direction='ltr',opacity=128,backgrount_as_image=False):
 
     if not backgrount_as_image:
-        size = 2000, 2000
-        max_text_size = int(size[0]*1.65)
-        txt = Image.new("RGBA", size, (255, 255, 255, 0))
+        size = (1000, 1000)
 
         text = input('Input text: ')
-        text_size = int(size[0]*1.65//len(text))
+        max_text_size = int(min(size)*1.65//10)
+        internal_length_of_line = int(min(size)*1.65//len(text))
+
+        if internal_length_of_line < max_text_size:
+            for i in range(1,len(text)//10+1):
+                text = text[:i*10+1] + '\n' + text[i*10+1:]
+
+        txt = Image.new("RGBA", size, (255, 255, 255, 0))
+        text_size = max_text_size//text_size_cust
 
         font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf", text_size)
 
